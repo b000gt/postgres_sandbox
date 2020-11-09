647e020d978c70c008bd21a94114f012d8e71849
Marc Scherrer <marc.scherrer@hsr.ch>
2020-11-09 13:12:35 +0100

    Add  guest

--- go.sh
+++ go.sh
@@ -2,19 +2,10 @@
 ret=5
 while [[ $ret != 0 ]]
 do
-    tmp=$(psql -U guest1 -p 5432 -h postgres-db -d eosmdbtwo)
+    tmp=$(psql -U guest -p 5432 -h postgres-db -d eosmdbtwo)
     ret=$?
 done
 
-echo "testing guest1"
+echo "testing guest"
 echo "------------------------------------"
-psql -U guest1 -p 5432 -h postgres-db -d eosmdbtwo -f psql-commands.sql
-echo "testing guest2"
-echo "------------------------------------"
-psql -U guest2 -p 5432 -h postgres-db -d eosmdbtwo -f psql-commands.sql
-echo "testing guest3"
-echo "------------------------------------"
-psql -U guest3 -p 5432 -h postgres-db -d eosmdbtwo -f psql-commands.sql
-echo "testing guest4"
-echo "------------------------------------"
-psql -U guest4 -p 5432 -h postgres-db -d eosmdbtwo -f psql-commands.sql
+psql -U guest -p 5432 -h postgres-db -d eosmdbtwo -f psql-commands.sql
